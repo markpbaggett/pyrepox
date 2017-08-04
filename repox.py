f041437219ed433d4d7d9d3300b9681097385330
Mark Baggett <mbagget1@utk.edu>
2017-08-04 16:07:02 -0400

    Add create method for provider class.

--- repox.py
+++ repox.py
@@ -53,7 +53,7 @@ class Aggregator:
 
 class Provider:
     def __init__(self, identifier, parent, name, name_code, country=None, country_code=None, description=None,
-                 home_page=None, provider_type=None, email=None):
+                 home_page=None, provider_type=None, email=None, headers=None):
         self.identifier = identifier
         self.aggregator = parent
         self.name = name
@@ -64,3 +64,43 @@ class Provider:
         self.home_page = home_page
         self.provider_type = provider_type
         self.email = email
+        if headers is None:
+            self.headers = {'content-type': 'application/json'}
+        else:
+            self.headers = headers
+
+    def create(self, username, password, url):
+        data = {"id": self.identifier, "name": self.name, "nameCode": self.name_code}
+        if self.country is not None:
+            data["country"] = self.country
+        if self.country_code is not None:
+            data["countryCode"] = self.country_code
+        if self.description is not None:
+            data["description"] = self.description
+        if self.name_code is not None:
+            data["nameCode"] = self.name_code
+        if self.home_page is not None:
+            data["homepage"] = self.home_page
+        if self.provider_type is not None:
+            data["providerType"] = self.provider_type
+        if self.email is not None:
+            data["email"] = self.email
+        r = requests.post("{}/{}".format(url, self.aggregator), data=json.dump(data), auth=(username, password),
+                          headers=self.headers)
+        if r.status_code == 201:
+            return "Successfully created provider {} for aggregator {}.".format(self.name, self.aggregator)
+        elif r.status_code == 400:
+            return "ERROR {}: Could not create {} provider for {} aggregator.  Invalid arguments."\
+                .format(r.status_code, self.name, self.aggregator)
+        elif r.status_code == 404:
+            return "ERROR {}: Could not create {} provider for {} aggregator. The aggregator does not exist."\
+                .format(r.status_code, self.name, self.aggregator)
+        elif r.status_code == 406:
+            return "ERROR {}: Could not create {} provider for {} aggregator. Missing arguments."\
+                .format(r.status_code, self.name, self.aggregator)
+        elif r.status_code == 409:
+            return "ERROR {}: Could not create {} provider for {} aggregator. The provider already exists."\
+                .format(r.status_code, self.name, self.aggregator)
+        else:
+            return "ERROR {}: Could not create {} provider for {} aggregator." \
+                .format(r.status_code, self.name, self.aggregator)
