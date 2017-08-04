c3f866d978325275f6f6df127d1a61a7608d797e
Mark Baggett <mbagget1@utk.edu>
2017-08-04 09:18:33 -0400

    Add Aggregator class and create method.

---
+++ repox.py
@@ -0,0 +1,38 @@
+import json
+import requests
+
+class Aggregator:
+    def __init__(self, identifier, name, name_code, home_page):
+        self.identifier = identifier
+        self.name = name
+        self.name_code = name_code
+        self.home_page = home_page
+
+    def __str__(self):
+        return self.name
+
+    def create(self, username, password, url):
+        headers = {'content-type': 'application/json'}
+        data = {"Aggregator":
+                    { "id": self.identifier, "name": self.name, "nameCode": self.name_code, "homepage": self.home_page}
+                }
+        r = requests.post(url, data=json.dumps(data), auth=(username, password), headers=headers)
+        if r.status_code == 201:
+            print("Successfully created aggregator.")
+        elif r.status_code == 409:
+            print("Could not create aggregator. It already exists.")
+        else:
+            print("There was a problem creating your aggregator.")
+
+
+class Provider:
+    def __init__(self):
+        self.identifier = ""
+        self.name = ""
+        self.country = ""
+        self.country_code = ""
+        self.description = ""
+        self.name_code = ""
+        self.home_page = ""
+        self.provider_type = ""
+        self.email = ""
