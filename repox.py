fc436e548869048a1ae853c3a414811ac99eafe4
Mark Baggett <mbagget1@utk.edu>
2018-12-11 10:52:00 -0500

    Add count records from dataset method.

--- repox.py
+++ repox.py
@@ -74,6 +74,10 @@ class Repox:
         return json.loads(requests.get(f"{self.swagger_endpoint}/datasets/{data_set_id}/date",
                                        auth=(self.username, self.password)).content)["result"]
 
+    def count_records_from_dataset(self, data_set_id):
+        return json.loads(requests.get(f"{self.swagger_endpoint}/datasets/{data_set_id}/count",
+                                       auth=(self.username, self.password)).content)["result"]
+
 
 if __name__ == "__main__":
     settings = yaml.load(open("settings.yml", "r"))
@@ -83,4 +87,4 @@ if __name__ == "__main__":
     #print(Repox(settings["url"], settings["username"], settings["password"]).get_list_of_providers("TNDPLAr0"))
     #print(Repox(settings["url"], settings["username"], settings["password"]).get_provider("utcr0"))
     #print(Repox(settings["url"], settings["username"], settings["password"]).get_list_of_sets_from_provider("utcr0"))
-    print(Repox(settings["url"], settings["username"], settings["password"]).get_last_ingest_date_of_set("p16877coll2"))
+    print(Repox(settings["url"], settings["username"], settings["password"]).count_records_from_dataset("p16877coll2"))
