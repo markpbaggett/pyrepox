1e78450712f03881d1eb9749c2ad5abf2f3a9f38
Mark Baggett <mbagget1@utk.edu>
2018-12-12 08:34:40 -0500

    Add typing.

--- repox.py
+++ repox.py
@@ -5,12 +5,12 @@ import yaml # Adding temporarily for testing only
 
 
 class Repox:
-    def __init__(self, repox_url, username, password):
+    def __init__(self, repox_url: str, username: str, password: str):
         self.swagger_endpoint = f"{repox_url}/repox/rest"
         self.username = username
         self.password = password
 
-    def list_all_aggregators(self, verbose=True):
+    def list_all_aggregators(self, verbose: bool=True) -> list:
         if verbose is True:
             return json.loads(requests.get(f"{self.swagger_endpoint}/aggregators",
                                        auth=(self.username, self.password)).content)
@@ -19,11 +19,11 @@ class Repox:
                                        auth=(self.username, self.password)).content)
             return [aggregator["id"] for aggregator in aggregators]
 
-    def get_specific_aggregator(self, aggregator_id):
+    def get_specific_aggregator(self, aggregator_id: str) -> dict:
         return json.loads(requests.get(f"{self.swagger_endpoint}/aggregators/{aggregator_id}",
                                        auth=(self.username, self.password)).content)
 
-    def get_aggregator_options(self):
+    def get_aggregator_options(self) -> dict:
         return json.loads(requests.get(f"{self.swagger_endpoint}/aggregators/options",
                                        auth=(self.username, self.password)).content)
 
@@ -36,7 +36,7 @@ class Repox:
     def delete_an_aggregator(self):
         return
 
-    def get_list_of_providers(self, aggregator_id, verbose=False):
+    def get_list_of_providers(self, aggregator_id: str, verbose: bool=False) -> any:
         if verbose is True:
             return json.loads(requests.get(f"{self.swagger_endpoint}/providers?aggregatorId={aggregator_id}",
                                        auth=(self.username, self.password)).content)
@@ -58,7 +58,7 @@ class Repox:
     def delete_provider(self):
         return
 
-    def get_list_of_sets_from_provider(self, provider_id, verbose=False):
+    def get_list_of_sets_from_provider(self, provider_id: str, verbose: bool=False) -> any:
         if verbose is True:
             return json.loads(requests.get(f"{self.swagger_endpoint}/datasets?providerId={provider_id}",
                                        auth=(self.username, self.password)).content)
@@ -67,28 +67,28 @@ class Repox:
                                        auth=(self.username, self.password)).content)
             return [data_set["dataSource"]["id"] for data_set in data_sets]
 
-    def get_dataset_details(self, data_set_id):
+    def get_dataset_details(self, data_set_id: str) -> dict:
         return json.loads(requests.get(f"{self.swagger_endpoint}/datasets/{data_set_id}",
                                        auth=(self.username, self.password)).content)
 
-    def get_last_ingest_date_of_set(self, data_set_id):
+    def get_last_ingest_date_of_set(self, data_set_id: str) -> str:
         return json.loads(requests.get(f"{self.swagger_endpoint}/datasets/{data_set_id}/date",
                                        auth=(self.username, self.password)).content)["result"]
 
-    def count_records_from_dataset(self, data_set_id):
+    def count_records_from_dataset(self, data_set_id: str) -> str:
         return json.loads(requests.get(f"{self.swagger_endpoint}/datasets/{data_set_id}/count",
                                        auth=(self.username, self.password)).content)["result"]
 
-    def get_statistics(self):
+    def get_statistics(self) -> dict:
         data = json.loads(requests.get(f"{self.swagger_endpoint}/statistics",
                                        auth=(self.username, self.password)).content)
         return json.dumps(xmltodict.parse(data["result"]))
 
-    def get_options_for_mappings(self):
+    def get_options_for_mappings(self) -> dict:
         return json.loads(requests.get(f"{self.swagger_endpoint}/mappings/options",
                                        auth=(self.username, self.password)).content)
 
-    def get_options_for_records(self):
+    def get_options_for_records(self) -> dict:
         return json.loads(requests.get(f"{self.swagger_endpoint}/records/options",
                                        auth=(self.username, self.password)).content)
 
@@ -96,7 +96,7 @@ class Repox:
         return json.loads(requests.get(f"{self.swagger_endpoint}/records?recordId={record_id}",
                                        auth=(self.username, self.password)).content)
 
-    def get_mapping_details(self, mapping_id):
+    def get_mapping_details(self, mapping_id) -> dict:
         return json.loads(requests.get(f"{self.swagger_endpoint}/mappings/{mapping_id}",
                                        auth=(self.username, self.password)).content)
 
@@ -110,4 +110,5 @@ if __name__ == "__main__":
     #print(Repox(settings["url"], settings["username"], settings["password"]).get_provider("utcr0"))
     #print(Repox(settings["url"], settings["username"], settings["password"]).get_list_of_sets_from_provider("utcr0"))
     #print(Repox(settings["url"], settings["username"], settings["password"]).count_records_from_dataset("p16877coll2"))
-    print(Repox(settings["url"], settings["username"], settings["password"]).get_mapping("UTKMODSrepaired"))
+    #print(Repox(settings["url"], settings["username"], settings["password"]).get_mapping("UTKMODSrepaired"))
+    print(type(Repox(settings["url"], settings["username"], settings["password"]).count_records_from_dataset("p16877coll2")))
