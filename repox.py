b5ebaf8b4623f195d8229202345f790607c7a549
Mark Baggett <mbagget1@utk.edu>
2018-12-13 00:00:11 -0500

    Add create aggregator method.

--- repox.py
+++ repox.py
@@ -9,19 +9,22 @@ class Repox:
         self.swagger_endpoint = f"{repox_url}/repox/rest"
         self.username = username
         self.password = password
+        self.headers = {'content-type': 'application/json'}
 
+    # Aggregators
     def list_all_aggregators(self, verbose: bool=True) -> list:
         """Returns all aggregators. If verbose is true, returns a list of dicts with metadata about aggregator.
-        If verbose is false, returns a list of aggregator ids as strings."""
+        If verbose is false, returns a list of aggregator ids as strings.
+        """
         if verbose is True:
             return json.loads(requests.get(f"{self.swagger_endpoint}/aggregators",
-                                       auth=(self.username, self.password)).content)
+                                           auth=(self.username, self.password)).content)
         else:
             aggregators = json.loads(requests.get(f"{self.swagger_endpoint}/aggregators",
-                                       auth=(self.username, self.password)).content)
+                                                  auth=(self.username, self.password)).content)
             return [aggregator["id"] for aggregator in aggregators]
 
-    def get_specific_aggregator(self, aggregator_id: str) -> dict:
+    def get_aggregator(self, aggregator_id: str) -> dict:
         """Takes an aggregator id and returns metadata about the aggregator as a dict."""
         return json.loads(requests.get(f"{self.swagger_endpoint}/aggregators/{aggregator_id}",
                                        auth=(self.username, self.password)).content)
@@ -30,24 +33,40 @@ class Repox:
         return json.loads(requests.get(f"{self.swagger_endpoint}/aggregators/options",
                                        auth=(self.username, self.password)).content)
 
-    def create_aggregator(self):
-        return
+    def create_aggregator(self, aggregator_id: str, aggregator_name: str, name_code: str="", homepage: str="") -> int:
+        """Requires an identifier and name of aggregator.  Optionally takes a name_code and a homepage.
+        If name_code is not specified, it's the same string as the aggregator_id. By default, the value of homepage is
+        blank.
 
-    def update_an_aggregator(self):
+        The HTTP status code is returned as an int.
+        """
+        if name_code == "":
+            name_code = aggregator_id
+        aggregator_data = {"id": aggregator_id,
+                           "name": aggregator_name,
+                           "nameCode": name_code,
+                           "homepage": homepage}
+        return type(requests.post(f"{self.swagger_endpoint}/aggregators",
+                                  auth=(self.username, self.password), headers=self.headers,
+                                  data=json.dumps(aggregator_data)).status_code)
+
+    def update_an_aggregator(self, aggregator_id):
         return
 
     def delete_an_aggregator(self):
         return
 
+    # Providers
     def get_list_of_providers(self, aggregator_id: str, verbose: bool=False) -> list:
         """Takes an aggregator id and returns the providers that belong to that aggregator as a list. If verbose is
-        true, metadata is included about each provider. If it is false, the list consists of provider ids as strings."""
+        true, metadata is included about each provider. If it is false, the list consists of provider ids as strings.
+        """
         if verbose is True:
             return json.loads(requests.get(f"{self.swagger_endpoint}/providers?aggregatorId={aggregator_id}",
-                                       auth=(self.username, self.password)).content)
+                                           auth=(self.username, self.password)).content)
         else:
             providers = json.loads(requests.get(f"{self.swagger_endpoint}/providers?aggregatorId={aggregator_id}",
-                                       auth=(self.username, self.password)).content)
+                                                auth=(self.username, self.password)).content)
             return [provider["id"] for provider in providers]
 
     def get_provider(self, provider_id: str) -> dict:
@@ -64,16 +83,18 @@ class Repox:
     def delete_provider(self):
         return
 
+    # Sets
     def get_list_of_sets_from_provider(self, provider_id: str, verbose: bool=False) -> list:
         """Takes a provider id and returns the data sets assoicated with it. If verbose is true, metadata about each
         set is included and the list consists of dicts.  If verbose is false, the list consists of dataset ids as
-        strings."""
+        strings.
+        """
         if verbose is True:
             return json.loads(requests.get(f"{self.swagger_endpoint}/datasets?providerId={provider_id}",
-                                       auth=(self.username, self.password)).content)
+                                           auth=(self.username, self.password)).content)
         else:
             data_sets = json.loads(requests.get(f"{self.swagger_endpoint}/datasets?providerId={provider_id}",
-                                       auth=(self.username, self.password)).content)
+                                                auth=(self.username, self.password)).content)
             return [data_set["dataSource"]["id"] for data_set in data_sets]
 
     def get_dataset_details(self, data_set_id: str) -> dict:
@@ -91,12 +112,14 @@ class Repox:
         return json.loads(requests.get(f"{self.swagger_endpoint}/datasets/{data_set_id}/count",
                                        auth=(self.username, self.password)).content)["result"]
 
+    # Statistics
     def get_statistics(self) -> dict:
         """Returns statistics about the entire Repox instance as a dict."""
         data = json.loads(requests.get(f"{self.swagger_endpoint}/statistics",
                                        auth=(self.username, self.password)).content)
         return json.dumps(xmltodict.parse(data["result"]))
 
+    # Mappings
     def get_options_for_mappings(self) -> dict:
         return json.loads(requests.get(f"{self.swagger_endpoint}/mappings/options",
                                        auth=(self.username, self.password)).content)
@@ -125,4 +148,7 @@ if __name__ == "__main__":
     #print(Repox(settings["url"], settings["username"], settings["password"]).get_list_of_sets_from_provider("utcr0"))
     #print(Repox(settings["url"], settings["username"], settings["password"]).count_records_from_dataset("p16877coll2"))
     #print(Repox(settings["url"], settings["username"], settings["password"]).get_mapping("UTKMODSrepaired"))
-    print(type(Repox(settings["url"], settings["username"], settings["password"]).count_records_from_dataset("p16877coll2")))
+    #print(Repox(settings["url"], settings["username"], settings["password"]).get_last_ingest_date_of_set("p16877coll2"))
+    #print(Repox(settings["url"], settings["username"], settings["password"]).get_record("urn:dpla.lib.utk.edu.country_mods:6e3b2417-b744-486a-827f-9fbbd81de0a6"))
+    print(Repox(settings["url"], settings["username"], settings["password"]).create_aggregator("test", "test"))
+    #print(Repox(settings["url"], settings["username"], settings["password"]).get_last_ingest_date_of_set("bcpl"))
