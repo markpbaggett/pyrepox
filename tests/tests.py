2456269b874a6d859443218ee7b63b82ac96c4fd
Mark Baggett <mbagget1@utk.edu>
2018-12-18 10:49:45 -0500

    Add unit test for get list of providers.

--- tests/tests.py
+++ tests/tests.py
@@ -3,7 +3,7 @@ import unittest
 from unittest.mock import patch
 
 
-def mocked_requests_get_list(*args, **kwargs):
+def mocked_aggregators_get_list(*args, **kwargs):
     class MockResponse:
         def __init__(self, response):
             self.content = response
@@ -13,14 +13,13 @@ def mocked_requests_get_list(*args, **kwargs):
 
     if args[0] is False:
         return MockResponse(["a", "b", "c", "d"]).json()
-
     elif args[0] is True:
         return MockResponse([{"key1": "a"}, {"key1": "b"}, {"key1": "c"}]).json()
 
     return MockResponse(None)
 
 
-def mocked_requests_get_dict(*args, **kwargs):
+def mocked_aggregators_get_dict(*args, **kwargs):
     class MockResponse:
         def __init__(self, response):
             self.content = response
@@ -37,12 +36,27 @@ def mocked_requests_get_dict(*args, **kwargs):
     return MockResponse(None)
 
 
-class RepoxTestAggregatorMethods(unittest.TestCase):
+def mocked_providers_get_list(*args, **kwargs):
+    class MockResponse:
+        def __init__(self, response):
+            self.content = response
+
+        def json(self):
+            return self.content
+
+    if kwargs["aggregator_id"] and kwargs["verbose"] is False:
+        return MockResponse(["provider1", "provider2", "provider3"]).json()
+
+    elif kwargs["aggregator_id"] == "abc" and kwargs["verbose"] is True:
+        return MockResponse([{"name": "provider1"}, {"name": "provider2"}, {"name": "provider3"}]).json()
+
+
+class RepoxTestGetAggregatorMethods(unittest.TestCase):
     def __init__(self, *args, **kwargs):
-        super(RepoxTestAggregatorMethods, self).__init__(*args, **kwargs)
+        super(RepoxTestGetAggregatorMethods, self).__init__(*args, **kwargs)
         self.request = Repox("http://localhost:8080", "admin", "admin")
 
-    @patch("repox.repox.Repox.list_all_aggregators", side_effect=mocked_requests_get_list)
+    @patch("repox.repox.Repox.list_all_aggregators", side_effect=mocked_aggregators_get_list)
     def test_list_aggregators(self, mock_get):
         repox_response = self.request.list_all_aggregators(False)
         self.assertIs(type(repox_response), list)
@@ -51,15 +65,30 @@ class RepoxTestAggregatorMethods(unittest.TestCase):
         self.assertIs(type(repox_response), list)
         self.assertIs(type(repox_response[0]), dict)
 
-    @patch("repox.repox.Repox.get_aggregator", side_effect=mocked_requests_get_dict)
+    @patch("repox.repox.Repox.get_aggregator", side_effect=mocked_aggregators_get_dict)
     def test_get_aggregator(self, mock_get):
         repox_response = self.request.get_aggregator("an_aggregator_id")
         self.assertIs(type(repox_response), dict)
 
-    @patch("repox.repox.Repox.get_aggregator_options", side_effect=mocked_requests_get_dict)
+    @patch("repox.repox.Repox.get_aggregator_options", side_effect=mocked_aggregators_get_dict)
     def test_get_aggregator_options(self, mock_get):
         repox_response = self.request.get_aggregator_options()
         self.assertIs(type(repox_response), dict)
+
+
+class RepoxTestGetProviderMethods(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super(RepoxTestGetProviderMethods, self).__init__(*args, **kwargs)
+        self.request = Repox("http://localhost:8080", "admin", "admin")
+
+    @patch("repox.repox.Repox.get_list_of_providers", side_effect=mocked_providers_get_list)
+    def test_get_list_of_providers(self, mock_get):
+        repox_response = self.request.get_list_of_providers(aggregator_id="abc", verbose=False)
+        self.assertIs(type(repox_response), list)
+        self.assertIs(type(repox_response[0]), str)
+        repox_response = self.request.get_list_of_providers(aggregator_id="abc", verbose=True)
+        self.assertIs(type(repox_response), list)
+        self.assertIs(type(repox_response[0]), dict)
 
 
 if __name__ == '__main__':
