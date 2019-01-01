8ff4c236c8a0af93c1f75d6f89530f45c4e52eec
Mark Baggett <mbagget1@utk.edu>
2019-01-01 17:07:01 -0500

    Switch to version 0.0.2.

--- setup.py
+++ setup.py
@@ -9,26 +9,20 @@ setup(
     description="a lightweight Repox client written in Python",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    version="0.0.1",
+    version="0.0.2",
     author="Mark Baggett",
     author_email="mbagget1@utk.edu",
     maintainer_email="mbagget1@utk.edu",
     url="https://github.com/markpbaggett/pyrepox",
     packages=find_packages(),
-    install_requires=[
-        "requests>=2.2.1",
-        "xmltodict>=0.11.0"
-    ],
+    install_requires=["requests>=2.2.1", "xmltodict>=0.11.0"],
     extras_require={
-        "docs": [
-            'sphinx >= 1.4',
-            "sphinxcontrib-napoleon >= 0.7"
-        ]
+        "docs": ["sphinx >= 1.4", "sphinxcontrib-napoleon >= 0.7"]
     },
     classifiers=[
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Programming Language :: Python :: 3.7",
-        "Operating System :: OS Independent"
+        "Operating System :: OS Independent",
     ],
-    keywords=["libraries", "dpla", "europeana", "aggregators"]
+    keywords=["libraries", "dpla", "europeana", "aggregators"],
 )
